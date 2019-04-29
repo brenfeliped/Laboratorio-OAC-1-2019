418591e8e2bdb52944954be526c568b6ce6a4a7a
RenanZX <renangodoimedeiros@gmail.com>
2019-04-28 22:20:13 -0300

    exercio 1.4 feito

---
+++ lab1/ex1.4/bmpdisplay.s
@@ -0,0 +1,39 @@
+#########################################################
+#  Programa de exemplo de uso do Bitmap Display Tool   	#
+#  OAC Abril 2019			  		#
+#  Marcus Vinicius Lamar		  		#
+#########################################################
+#
+# Use o programa paint.net (baixar da internet) para gerar o arquivo .bmp de imagem 320x240 e 24 bits/pixel 
+# para então usar o programa bmp2isc.exe para gerar o arquivo .s correspondente para colocar no include
+
+.data
+.include "bandeira_do_brasil.s"
+
+.text
+
+# Preenche a tela de vermelho
+	li t1,0xFF000000	# endereco inicial da Memoria VGA
+	li t2,0xFF012C00	# endereco final 
+	li t3,0x88888888  	# cor azul|azul|azul|azul
+LOOP: 	beq t1,t2,FORA		# Se for o último endereço então sai do loop
+	sw t3,0(t1)		# escreve a word na memória VGA
+	addi t1,t1,4		# soma 4 ao endereço
+	j LOOP			# volta a verificar
+
+
+# Carrega a imagem1
+FORA:	li t1,0xFF000000	# endereco inicial da Memoria VGA
+	li t2,0xFF012C00	# endereco final 
+	la s1,bandeira_do_brasil # endereço dos dados da tela na memoria
+	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
+LOOP1: 	beq t1,t2,FIM		# Se for o último endereço então sai do loop
+	lw t3,0(s1)		# le um conjunto de 4 pixels : word
+	sw t3,0(t1)		# escreve a word na memória VGA
+	addi t1,t1,4		# soma 4 ao endereço
+	addi s1,s1,4
+	j LOOP1			# volta a verificar
+	
+# devolve o controle ao sistema operacional
+FIM:	li a7,10		# syscall de exit
+	ecall
